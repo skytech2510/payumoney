ada5839b8fa48e152b2c32543b79852c2951c2ad
skytech2510 <skytech2510@gmail.com>
2024-10-25 12:28:16 +0700

    update

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 /*
+|
 |--------------------------------------------------------------------------
 | Check If Application Is Under Maintenance
 |--------------------------------------------------------------------------
